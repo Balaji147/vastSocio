971fc4d3fd8a42f81cb629fe5086223490086a0c
Balaji147 <mebala147@gmail.com>
2023-05-16 17:12:08 +0530

    final commit

--- frontend/src/__test__/post.test.js
+++ frontend/src/__test__/post.test.js
@@ -1,7 +1,28 @@
-import Enzyme from "enzyme"
-import { EnzymeAdapter } from "enzyme"
+import React from "react"
+import {render as rtl, fireEvent, screen} from "@testing-library/react"
+// import {Provider} from "react-redux"
+import Post from "../Post/Post"
+// import { Component } from "react"
 
-Enzyme.configure({
-    adapter:new EnzymeAdapter(),
-    disableLifecycleMethods:true
+// const render = component=>{
+//  <Provider store={store}>
+//     {component}
+// </Provider>
+   
+// }
+
+
+test('handle Like', () => {
+    render(<Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...options}>
+          <App />
+        </AlertProvider>
+      </Provider>)
+
+    const counter = screen.getByTestId("counter")
+    const incrementBtn =screen.getByTestId("increment")
+
+    fireEvent.click(incrementBtn)
+
+    expect(counter).toHaveTextContent(true)
 })
